246fed42da89649a964ce873d51d1722617b63ec
irramail <irramail@gmail.com>
2020-01-08 23:20:57 +0700

    old staff

--- serverside_sync_time.sh
+++ serverside_sync_time.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for id in `mysql -B -N -u YouLogin -pYouPass krms -e 'select id from YouTable where (now() - tl_answ + 40000) > 60;'`
+for id in `mysql -B -N -u YouLogin -pYouPass YouDB -e 'select id from YouTable where (now() - tl_answ + 40000) > 60;'`
 do
-  mysql -B -N -u YouLogin -pYouPass krms -e "update YouTable set status=0 where id_client = $id and type = 'SYNC.TIME' order by id desc limit 1;"
+  mysql -B -N -u YouLogin -pYouPass YouDB -e "update YouTable set status=0 where id_client = $id and type = 'SYNC.TIME' order by id desc limit 1;"
 #  echo "$id $(date)" >> ~/logs/$(date "+%F")".log"
 done
 
